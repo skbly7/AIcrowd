d23eb4c5dfbc3d88f0dc2634c9b3b6e5aeec342d
Nate Jenkins <nate.jenkins@gmail.com>
2019-01-12 03:41:28 -0800

    cleanup

--- app/controllers/participants/omniauth_callbacks_controller.rb
+++ app/controllers/participants/omniauth_callbacks_controller.rb
@@ -1,18 +1,6 @@
 class Participants::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = Participant.from_omniauth(request.env["omniauth.auth"])
-    if @user.persisted?
-      sign_in_and_redirect @user
-      set_flash_message(:notice, :success, kind: "Github") if is_navigational_format?
-    else
-      puts "NEW USER"
-      session["devise.github_data"] = request.env["omniauth.auth"]
-      redirect_to new_participant_registration_url
-      ### NATE: had to use this route locally as the base url
-      ### was getting set to localhost and I'm not sure why.
-      # redirect_to 'http://aicrowd.com:3001/participants/sign_up'
-    end
+    oauth2_generic
   end
 
   def oauth2_generic
@@ -29,11 +17,8 @@ class Participants::OmniauthCallbacksController < Devise::OmniauthCallbacksContr
     else
       puts "NEW USER"
       Rails.logger.info request.env["omniauth.auth"]
-      session["devise.crowdai_data"] = request.env["omniauth.auth"]
-      redirect_to new_participant_registration_url
-      ### NATE: had to use this route locally as the base url
-      ### was getting set to localhost and I'm not sure why.
-      # redirect_to 'http://aicrowd.com:3001/participants/sign_up'
+      session["devise.omniauth_data"] = request.env["omniauth.auth"]
+      redirect_to new_participant_registration_path
     end
   end
 
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -211,17 +211,6 @@ class Participant < ApplicationRecord
   def self.from_omniauth(auth)
     puts "FROM OMNIAUTH:"
     puts auth
-    ### NATE: this is the standard workflow from https://github.com/plataformatec/devise/wiki/OmniAuth:-Overview
-    ### however we want one user per email
-    # where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    #   user.email = auth.info.email
-    #   user.password = Devise.friendly_token[0,20]
-    #   user.name = auth.info.name.gsub(/\s+/, '_')
-    #   # user.image = auth.info.image # assuming the user model has an image
-    #   # If you are using confirmable and the provider(s) you use validate emails,
-    #   # uncomment the line below to skip the confirmation emails.
-    #   # user.skip_confirmation!
-    # end
     raw_info = auth.raw_info || (auth.extra && auth.extra.raw_info.participant)
     puts "RAW_INFO:"
     puts raw_info
